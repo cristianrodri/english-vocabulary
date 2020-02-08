c9064aa2ac144fe7e74b27d26f0060d3326b8ee7
Cristian Rodriguez <carlo.interpab@gmail.com>
2020-02-08 14:00:38 -0300

    Fixing focus error and adding local storage to know what page I'm studying

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,22 @@ import orderVocabulary from './order-vocabulary'
 import { hearVocabulary } from './hear-vocabulary'
 import { practice } from './practice'
 
+{
+  // adding color to the card that I will be studying
+  console.log(localStorage.getItem('study-page'))
+
+  const name = Array.from(document.querySelectorAll('.main-card'))
+
+  name.some(childDOM => {
+    const childName = childDOM.querySelector('.main-card__name')
+
+    if (childName.textContent === localStorage.getItem('study-page')) {
+      return childName.classList.add('u-current-study-page')
+    }
+  })
+
+}
+
 orderVocabulary()
 hearVocabulary()
 practice()
--- src/js/practice.js
+++ src/js/practice.js
@@ -26,10 +26,10 @@ const changeUI = (lang, opposite) => {
   shownInput.parentElement.parentElement.classList.add('container-focused')
 
   // check the input if word is correct
-  checkVocabulary(lang)
+  checkVocabulary()
 }
 
-const checkVocabulary = lang => {
+const checkVocabulary = () => {
 
   const allShownInputs = document.querySelectorAll('.show-input')
 
@@ -57,73 +57,87 @@ const checkVocabulary = lang => {
     }
   })
 
-  document.addEventListener('keydown', async e => {
+}
 
+const containerTable = document.getElementById('container-table')
+
+if (containerTable) {
+
+  containerTable.addEventListener('keydown', async e => {
+  
     // enter the input and check is value is equal to the word in the table
     if (e.key === 'Enter' && e.target.tagName === 'INPUT') {
-
+  
+      const header = document.querySelector('.main-header__title')
+  
       const word = e.target.parentElement.childNodes[0].textContent.toLowerCase()
       const value = e.target.value
+      // const target = e.target
       const isEnglishWord = e.target.parentElement.className.includes('eng-word')
-
+  
       // input value is correct
       if (word === value) {
         e.target.classList.remove('show-input')
         e.target.nextElementSibling.classList.remove('show-input')
-
+  
+        // save in local storage after typing a word meaning that this is the current page I'm studying
+        localStorage.removeItem('study-page')
+        localStorage.setItem('study-page', header.textContent)
+  
         const firstInputOfThePage = document.querySelectorAll('.show-input')[0]
-
+  
         // The page has more than one english translation
         const hasMoreThanOneEnglishTranslation = e.path[2].children
-
+  
         // english translation has more than 1 word ("eng" and "es" have more than 2 words by column) && practice is in english && the next input is shown
-        if (hasMoreThanOneEnglishTranslation.length > 2 && lang === 'eng' && e.target.parentElement.nextElementSibling.children[0].className.includes('show-input')) {
+        if (hasMoreThanOneEnglishTranslation.length > 2 && inEnglishWord && e.target.parentElement.nextElementSibling.children[0].className.includes('show-input')) {
           const nextInputSibling = e.target.parentElement.nextElementSibling.children[0]
-
+  
           // if next input is shown, focus it. Otherwise focus the first shown input of the page
           nextInputSibling.className.includes('show-input')
             ? nextInputSibling.focus()
             : firstInputOfThePage.focus()
-
+  
         // otherwise is spanish practice or the table only has 2 columns and check has next sibling container
         } else if (e.path[2].nextElementSibling) { // the container-table__word has sibling
           const nextInput = e.path[2].nextElementSibling.querySelector('.show-input')
-
+  
           // if next input is shown, focus it. Otherwise focus the first shown input of the page
           nextInput
             ? nextInput.focus()
             : firstInputOfThePage.focus()
-
+  
         } else {
           // otherwise is the last shown input of the page and after will focus the first shown input
           if (firstInputOfThePage) firstInputOfThePage.focus()
         }
-
+  
         // empty input
         e.target.value = ''
         e.target.classList.remove('add-focus-error')
-
+  
         // hear voice
         if ('speechSynthesis' in window && isEnglishWord) {
           const voices = await getAPIVoices()
           const synth = window.speechSynthesis
           const word = new SpeechSynthesisUtterance(value)
-
+  
           word.voice = voices[1]
           synth.speak(word)
         }
-
+  
       } else {
-        e.target.classList.add('add-focus-error')
+        if (!e.target.className.includes('add-focus-error')) e.target.classList.add('add-focus-error')
       }
     }
-
+  
     if (e.key === 'Escape' && e.target.tagName === 'INPUT') {
       e.target.value = ''
       e.target.classList.remove('add-focus-error')
     }
   })
 }
+
 
 export const practice = () => {
   const english = document.querySelector('.practice-english')
