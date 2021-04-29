5e74a5a0fa4d60837c4c59d3bd1e4782157c31c4
Cristian Rodriguez <contacto.cristianrodriguez@gmail.com>
2021-04-29 12:52:47 -0300

    add context provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,31 @@
 import { AppProps } from 'next/app'
-import React from 'react'
+import React, { createContext, useState } from 'react'
 import '../styles/styles.css'
 
+interface IContext {
+  showColumnInputs: number
+  showInputs: (column: number) => void
+}
+
+const contextDefaultValues: IContext = {
+  showColumnInputs: -1,
+  showInputs: () => {}
+}
+
+const GlobalContext = createContext<IContext>(contextDefaultValues)
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  const [showColumnInputs, setShowColumnInput] = useState<number>(-1)
+
+  const showInputs = (columnIndex: number) => {
+    setShowColumnInput(columnIndex)
+  }
+
+  return (
+    <GlobalContext.Provider value={{ showColumnInputs, showInputs }}>
+      <Component {...pageProps} />
+    </GlobalContext.Provider>
+  )
 }
 
 export default MyApp
