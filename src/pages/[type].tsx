a2a27b30b91b7f2df960d46486419785dd78585b
Cristian <contacto.cristianrodriguez@gmail.com>
2022-03-30 10:52:21 -0300

    get data from getStaticProps instead of getServerSideProps in [type] component

--- src/pages/[type].tsx
+++ src/pages/[type].tsx
@@ -1,30 +1,45 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import Layout from '../components/Layout'
 import { customTitle } from '../utils/strings'
 import { Container } from '../components/table/Container'
-import { getSheetData } from './../services/sheets'
+import { getSheetData, getSheetNames } from './../services/sheets'
 import { Context } from '../context/GlobalContext'
 export interface StaticProps {
   title: string
   words: string[][]
-  columnNames: string[]
+  langColumns: string[]
 }
 
-export const getServerSideProps: GetServerSideProps<
+// Get all titles from Sheet names
+export const getStaticPaths: GetStaticPaths = async () => {
+  const titles = await getSheetNames()
+
+  const paths = titles.map(title => ({ params: { type: title } }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps<
   StaticProps,
   { type: string }
 > = async context => {
   const titleParams = context.params?.type as string
-  const { words, columnNames, title } = await getSheetData(titleParams)
-  return { props: { title, words, columnNames } }
+
+  // Get all data from specific type of word on the Sheet
+  const { words, langColumns, title } = await getSheetData(titleParams)
+
+  return { props: { title, words, langColumns }, revalidate: 1 }
 }
 
-const VocabularyType = ({ title, words, columnNames }: StaticProps) => {
-  const titleCaptalized = customTitle(title)
+const VocabularyType = ({ title, words, langColumns }: StaticProps) => {
+  const customizedTitle = customTitle(title)
 
   return (
-    <Context data={words} columnNames={columnNames}>
-      <Layout title={titleCaptalized} banner={title}>
+    <Context data={words} langColumns={langColumns}>
+      <Layout title={customizedTitle} banner={title}>
         <Container />
       </Layout>
     </Context>
