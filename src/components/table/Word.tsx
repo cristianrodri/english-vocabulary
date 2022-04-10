6ace90d7450a60da1d889893515d21475e3bacb8
Cristian <contacto.cristianrodriguez@gmail.com>
2022-04-10 08:50:24 -0400

    remove unnecessary import

--- src/components/table/Word.tsx
+++ src/components/table/Word.tsx
@@ -2,7 +2,6 @@ import { useContext } from 'react'
 import styled from 'styled-components'
 import { GlobalContext } from '@context/GlobalContext'
 import { speechSynthesisEng, speechSynthesisSpa } from '@utils/speechSynthesis'
-import { capitalizedText } from '@utils/strings'
 
 interface Props {
   word: string
